d35d684457a2723c11e16741a3093b5b1cf23547
kurone-kito <krone@kit.black>
2019-07-31 21:54:32 +0900

    Implemented the update for NodeJS on Windows

--- bin/_update.admin.ps1
+++ bin/_update.admin.ps1
@@ -1,3 +1,12 @@
 Set-StrictMode -Version Latest
 
 choco upgrade -y all
+
+nodist + 8
+nodist + 10
+nodist + 12
+nodist global 12
+
+npm upgrade -g
+npm install -g npm
+npm install -g exp serverless windows-build-tools yarn
