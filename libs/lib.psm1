f02065a6b1993d3a4bfd0f56c15917205858804e
kurone-kito <krone@kit.black>
2022-06-18 20:42:03 +0900

    refactor: moved the feature that gives privileges to PowerShell side

--- libs/lib.psm1
+++ libs/lib.psm1
@@ -47,5 +47,67 @@ function Add-Links {
   #>
 }
 
+function Get-IsAdmin {
+  $user = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+  $principal = [Security.Principal.WindowsPrincipal]$user
+  $principal.IsInRole('Administrators')
+  <#
+  .SYNOPSIS
+  The function gets whether the current user has privileges.
+  .INPUTS
+  None.
+  .OUTPUTS
+  System.Boolean. Whether the current user has privileges.
+  #>
+}
+
+function Invoke-Self {
+  $options = Join-PSOptions $MyInvocation.ScriptName
+  Start-Process powershell.exe -ArgumentList $options -Wait
+  <#
+  .SYNOPSIS
+  The function invokes the caller script on the new window.
+  .INPUTS
+  None.
+  #>
+}
+
+function Invoke-SelfWithPrivileges {
+  if (Get-IsAdmin) {
+    return $false
+  }
+  [Console]::WriteLine('Please elevate to privileges for installing an app')
+  $options = Join-PSOptions $MyInvocation.ScriptName
+  Start-Process powershell.exe -ArgumentList $options -Wait -Verb RunAs
+  return $true
+  <#
+  .SYNOPSIS
+  The function invokes the caller script on the new window with elevate to privileges.
+  .INPUTS
+  None.
+  .OUTPUTS
+  System.Boolean. It returns true when it should exit to the caller script.
+  Otherwise, returns false.
+  #>
+}
+
+function Join-PSOptions {
+  param (
+    [Parameter(Mandatory)][string]
+    # Specifies the filename.
+    $fileName
+  )
+  '-ExecutionPolicy Bypass -NoLogo -File "{0}" 1' -f $fileName
+  <#
+  .SYNOPSIS
+  The function gets the options on PowerShell execution.
+  .OUTPUTS
+  System.String. The options.
+  #>
+}
+
 Export-ModuleMember -Function Add-Link
 Export-ModuleMember -Function Add-Links
+Export-ModuleMember -Function Get-IsAdmin
+Export-ModuleMember -Function Invoke-Self
+Export-ModuleMember -Function Invoke-SelfWithPrivileges
--- setup_.ps1
+++ setup_.ps1
@@ -6,6 +6,17 @@ Import-Module -Name ./libs/lib.psm1
 
 Get-ChildItem -Recurse libs/*.ps1 | Unblock-File
 
+if (Invoke-SelfWithPrivileges) {
+  exit
+}
+
+if (-not $args.Count) {
+  Invoke-Self
+  exit
+}
+
+Get-ChildItem -Recurse *.ps1 | Unblock-File
+
 ### Link to dotfile for home dir
 Get-ChildItem -Attributes !Directory `
   | Where-Object { $_.Name -match '^\.' } `
