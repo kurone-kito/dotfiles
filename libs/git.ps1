cf0b3305d122dab76675e2acd508bd7e9000b157
kurone-kito <krone@kit.black>
2022-07-01 07:27:22 +0900

    refactor: splitted the git and gpg configurations

---
+++ libs/git.ps1
@@ -0,0 +1,11 @@
+#!/usr/bin/env pwsh
+
+Set-StrictMode -Version Latest
+Set-Location $PSScriptRoot
+Import-Module -Name ./lib.psm1
+
+### Setup Git
+Set-Location ..
+$GPGPath = (Get-Command -Name gpg).Source
+Copy-Item -Path .\templates\.gitconfig -Destination $env:USERPROFILE -Force
+git config --global gpg.program $GPGPath
--- setup_.ps1
+++ setup_.ps1
@@ -25,13 +25,5 @@ Get-ChildItem -Force -Attributes !Directory `
 ./libs/cmdrc.ps1
 ./libs/pwshrc.ps1
 ./libs/terminal.ps1
-
-### Setup GPG
-$GPGHome = Join-Path $env:APPDATA -ChildPath gnupg
-Add-Links -Source .gnupg -Destination $GPGHome
-gpgconf --kill gpg-agent
-
-### Setup Git
-$GPGPath = (Get-Command -Name gpg).Source
-Copy-Item -Path .\templates\.gitconfig -Destination $env:USERPROFILE -Force
-git config --global gpg.program $GPGPath
+./libs/gpg.ps1
+./libs/git.ps1
