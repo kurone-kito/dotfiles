f77b5a0085b258991369df80c9acb72c7de4adaf
kurone-kito <krone@kit.black>
2022-08-13 12:45:50 +0900

    fix: fixed a bug that corrupted Windows Terminal settings
    In the case of an environment where Windows Terminal is not installed,
    I implemented skipping the setting itself.

--- libs/terminal.ps1
+++ libs/terminal.ps1
@@ -6,14 +6,18 @@ Import-Module -Name ./lib.psm1
 
 Write-Output 'Setup the Windows Terminal configurations'
 Set-Location ..
-
-if (Get-Command jq) {
+if ((Get-Command jq) -and (Get-Command wt)) {
+  $process = Start-Process wt -PassThru
   $src = Join-Path templates 'windowsTerminal.settings.json'
   $dst = $env:LOCALAPPDATA `
     | Join-Path -ChildPath Packages `
     | Join-Path -ChildPath Microsoft.WindowsTerminal_8wekyb3d8bbwe `
     | Join-Path -ChildPath LocalState `
     | Join-Path -ChildPath settings.json
+  while (-not (Test-Path $dst)) {
+    Start-Sleep 3
+  }
+  Stop-Process -InputObject $process -Force -ErrorAction SilentlyContinue
   . ./.pwsh.profile.d/fnm.ps1
   npx -y strip-json-comments-cli $dst `
     | ConvertFrom-Json `
