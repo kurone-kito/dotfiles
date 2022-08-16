0f5e87670fa13bac29b152fec06d2e0416836307
kurone-kito <krone@kit.black>
2022-08-16 13:51:03 +0900

    feat: added the update scripts

--- libs/link.ps1
+++ libs/link.ps1
@@ -11,4 +11,7 @@ Get-ChildItem -Force -Attributes !Directory `
   | Where-Object { $_.Name -match '^\.' } `
   | ForEach-Object { $_ | Add-Link -Destination $env:USERPROFILE }
 
+$BinHome = Join-Path $HOME bin
+Add-Links -Source bin -Destination $BinHome
+
 Pop-Location
