9b134b9924b796bde14faa7a3d9134caa5a21df8
kurone-kito <krone@kit.black>
2023-02-21 13:19:16 +0900

    fix: fixed a bug that prevented path resolution for symbolic links from working properly

--- libs/lib.sh
+++ libs/lib.sh
@@ -1,16 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+link_to() {
+  DST_PATH="${2}/$(basename "${1}")"
+  SRC_PATH="$(realpath "${1}")"
+  ln -snf "${SRC_PATH}" "${DST_PATH}"
+}
+
+export -f link_to
 
 deploy() {
   NAME=$1
   SRC=$2
   [ $# -ge 3 ] && DST=$3 || DST=$2
-  DSTPATH="${HOME}/${DST}"
+  DST_PATH="$(realpath "${HOME}/${DST}")"
   mkdir -p "${DSTPATH}"
   find "${SRC}" -depth 1 -type f -name "${NAME}" -print0 \
-    | xargs -0 -n1 basename \
-    | xargs -I {} ln -snf \
-      "$(realpath "$(pwd)/${SRC}/{}")" \
-      "$(realpath "${DSTPATH}/{}")"
+    | xargs -0 -n1 \
+    | xargs -I {} bash -c "link_to {} ${DST_PATH}"
 }
 
 deploy_with_chmod() {
